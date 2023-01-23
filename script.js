113119d351e7529d57c51472a2342ed50931532f
Mohammad Mubarak <115584175+Mohammad-Mubarak@users.noreply.github.com>
2023-01-23 19:56:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your code here
+// Name*,
+// Email*
+// Gender* (radio buttons),
+// Car Brands (options : [BMW, Audi, Mercedez])
+// Age*
+// DOB*
+// Password*
+// submit button.
+
+// * are required fields,
+// Use method attribute value that you should use in this case for the form!
+// The respective input elements for the mentioned fields should have ids #name, #email, #password, #cars, #age, #dob. For example, The element for selecting car brand should have id #cars
+// The radio buttons for gender should have name attribute as gender.
+// All input elements must also have corresponding labels with for attribute set to id's of element they belong. For example, the label for input of name will be <label for="name"> Name</label>
